c2acb8ae8915d10eba73ee04335dbef1fe0af308
John Johnson <jbj1996@gmail.com>
2021-09-30 14:30:29 -0400

    added old school http request and bolstered security of new style request

--- Node.js.V2/reddit-cli/reddit.mjs
+++ Node.js.V2/reddit-cli/reddit.mjs
@@ -6,15 +6,24 @@ import yargs from 'yargs'
 console.log(process.argv)
 const { argv }  = yargs(process.argv)
 
-const res = await fetch('https://reddit.com/.json')
-const data = await res.json()
-const children = data.data.children
-const randomPost = children[Math.floor(Math.random() * children.length)]
-const link = `https://reddit.com${randomPost.data.permalink}`
+//old school fetch request: without async
+// fetch('http://reddit.com/.json').then(res => {
+//         res.json().then(data => {
+//             console.log(data)
+//         }).catch(e => {
+//             console.log("error:", e)
+//         })
+// })
+
+const res = await fetch('https://reddit.com/.jon')
+const data = await res.json().catch(e => ({data: {}}))
+const children = data?.data?.children ?? []
+const randomPost = children[Math.floor(Math.random() * children?.length)]
+const link = `https://reddit.com${randomPost?.data?.permalink}`
 
 if (argv.print) {
     console.log(`
-    title: ${randomPost.data.title}
+    title: ${randomPost?.data?.title}
     link:  ${link}
     `)
 } else {
