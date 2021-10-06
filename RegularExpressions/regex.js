3bfaa5e8bb4cababbfce33b568dc13b1e676ccb9
John Johnson <jbj1996@gmail.com>
2021-10-06 11:44:22 -0400

    added more examples for the simple regex patterns, excluding replaceAll which seems to be obsolete

--- RegularExpressions/regex.js
+++ RegularExpressions/regex.js
@@ -1,8 +1,9 @@
 let example1 = "The latest airplane designs evolved from slabcraft."
 let example2 = "Hi, do you know your abc's?"
 let example3 = "Grab crab"
+let example4 = "abcra crab dabcra"
 
-let examples = [example1, example2, example3]
+let examples = [example1, example2, example3, example4]
 let re = /abc/
 let reg = new RegExp(/abc/,  'g')
 
@@ -12,4 +13,14 @@ examples.map(example => {console.log(example.match(re))})
 examples.map(example => {
   console.log(example.matchAll(reg))
 })
-// examples.map(example => {console.log(example.search(re))})
+examples.map(example => {
+  console.log(example.search(reg))
+})
+examples.map(example => {
+  console.log(example.replace(reg, 'flomp'))
+})
+// examples.map(example => {
+//   console.log(example.replaceAll(reg, 'flomp'))
+// })
+examples.map(example => {console.log(example.split(re))}) //split omits the args you provide from the array, it seems.
+
