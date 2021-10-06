a84f654fa9c0f68dacbe5182c3c688f3d0ced462
John Johnson <jbj1996@gmail.com>
2021-10-06 12:40:03 -0400

    began toying with iterators

--- RegularExpressions/regex.js
+++ RegularExpressions/regex.js
@@ -5,24 +5,37 @@ let example4 = "abcra crab dabcra"
 //regex's are case sensitive
 let example5 = "Abcra crab dabcra"
 
-let examples = [example1, example2, example3, example4]
+let examples = [example1, example2, example3, example4, example5]
 let re = /abc/
 let reg = new RegExp(/abc/,  'g')
 
 examples.map(example => {console.log(re.test(example))})
 examples.map(example => {console.log(re.exec(example))})
 examples.map(example => {console.log(example.match(re))})
-examples.map(example => {
-  console.log(example.matchAll(reg))
-})
+
+// Iterators can only be looped over once
+let iterator = example4.matchAll(reg);
+
+// This will consume all the elements of the iterator
+console.log(iterator.next());
+console.log(iterator.next());
+
+for (let match of iterator) {
+  console.log('???', match)
+}
+
+for (let match of iterator) {
+  console.log('!!!', match)
+}
+
 examples.map(example => {
   console.log(example.search(reg))
 })
 examples.map(example => {
   console.log(example.replace(reg, 'flomp'))
 })
-// examples.map(example => {
-//   console.log(example.replaceAll(reg, 'flomp'))
-// })
+examples.map(example => {
+  console.log(example.replaceAll(reg, 'flomp'))
+})
 examples.map(example => {console.log(example.split(re))}) //split omits the args you provide from the array, it seems.
 
