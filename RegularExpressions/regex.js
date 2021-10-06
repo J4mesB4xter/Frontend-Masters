7101973d124a15269afeeebd3280b7280f679249
John Johnson <jbj1996@gmail.com>
2021-10-06 11:20:03 -0400

    added exec and match regex uses

--- RegularExpressions/regex.js
+++ RegularExpressions/regex.js
@@ -5,3 +5,7 @@ let example3 = "Grab crab"
 let examples = [example1, example2, example3]
 let re = /abc/
 examples.map(example => {console.log(re.test(example))})
+examples.map(example => {console.log(re.exec(example))})
+examples.map(example => {console.log(example.match(re))})
+// examples.map(example => {console.log(example.matchAll(re))})
+// examples.map(example => {console.log(example.search(re))})
